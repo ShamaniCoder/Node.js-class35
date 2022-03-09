dd6640cb9910a98340eec2194b172312b71e81e3
ShamaniCoder <ertugrulaktn@gmail.com>
2022-03-09 01:01:27 +0100

    added changes to test file

--- homework/HackYour Temperature/__test__/app.test.js
+++ homework/HackYour Temperature/__test__/app.test.js
@@ -5,6 +5,12 @@ const request = supertest(app);
 
 describe("POST /weather", () => {
   // Successful Cases
+  test("with a valid city name it should respond result", async () => {
+    const response = await request
+      .post("/weather")
+      .send({ cityName: "Amsterdam" });
+    expect(response.body.weatherText).toBeDefined();
+  });
   test("should respond with a 200 status code for correct city name", async () => {
     const response = await request
       .post("/weather")
@@ -19,14 +25,8 @@ describe("POST /weather", () => {
       expect.stringContaining("json")
     );
   });
-  test("with a valid city name it should respond result", async () => {
-    const response = await request
-      .post("/weather")
-      .send({ cityName: "leiden" });
-    expect(response.body.weatherText).toBeDefined();
-  });
 
-  // Error Cases
+  // Unsuccessful Cases
   test("should respond with a 400 status code if empty object send", async () => {
     const response = await request.post("/weather").send({});
     expect(response.statusCode).toBe(400);
